10b575c69d91c549f2b212a0c6158c3d80d1eab0
p2622948 <p2622948@GWHXX473WIN017W>
2022-02-11 15:06:21 +0000

    rgfb

---
+++ Team_INFINITY_project/Team_INFINITY_project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Team_INFINITY_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
