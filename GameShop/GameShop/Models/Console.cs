65e2b58a804c66834a2a7ec614ea331725232f00
p2622948 <p2622948@GWHXX473WIN017W>
2022-03-23 18:48:29 +0000

    k

---
+++ GameShop/GameShop/Models/Console.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GameShop.Models
+{
+    public class Console
+    {
+        public virtual int ConsoleId { get; set; }
+        public virtual string ConsoleName { get; set; }        
+    }
+}
---
+++ Team_INFINITY_project/Elegant College/Models/Console.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Elegant_College.Models
+{
+    public class Console
+    {
+        public virtual int ConsoleID { get; set; }
+
+        public virtual string ConsoleName { get; set; }
+    }
+}
