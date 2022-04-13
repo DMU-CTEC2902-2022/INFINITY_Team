d0d5c65561991db1e9cbfc07df258e292e8d183f
Frizzle15 <98060609+Frizzle15@users.noreply.github.com>
2022-04-13 15:35:19 +0100

    UserController.cs created

---
+++ Team_INFINITY_project/Elegant College/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using Elegant_College.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Elegant_College.Controllers
+{
+    public class UserController
+    {
+        public ActionResult AddEdit(int id = 0)
+        {
+            User user = new User();
+            return View(user);
+
+        }
+
+
+    }
+}
--- Team_INFINITY_project/Elegant College/Models/User.cs
+++ Team_INFINITY_project/Elegant College/Models/User.cs
@@ -5,19 +5,15 @@ using System.Web;
 
 namespace Elegant_College.Models
 {
-    public class User
+    public partial class User
     {
         public int UserID { get; set; }
-        [DisplayName("User Name")]
-        [Required{ErrorMessage = "This field cannot be null")]
+       
         public string Username { get; set; }
-        [DataType(DataType.Password)]
-        [Required(ErrorMessage = "This field is required")]
+        
 
         public string Password { get; set; }
-        [DisplayName("Confirm Password")]
-        [DataType(DataType.Password)]
-        [Comapre("Password")]
+        
         public string ConfirmPassword { get; set; }
         public bool Admin { get; set; }
     }
