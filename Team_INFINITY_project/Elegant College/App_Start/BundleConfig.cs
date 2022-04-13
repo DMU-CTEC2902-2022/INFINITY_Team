e7166f44e0f07c87e6817fc90ecc978b7bb01132
p2622948 <p2622948@GWHXX473WIN017W>
2022-04-13 12:54:24 +0100

    kartik

--- Team_INFINITY_project/Elegant College/App_Start/BundleConfig.cs
+++ Team_INFINITY_project/Elegant College/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Elegant_College
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
